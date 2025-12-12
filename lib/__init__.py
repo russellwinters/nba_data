5494d69f4a87db2a749f8573b375348997711d1b
Russell Winters <russellwinters@protonmail.com>
2025-12-11 22:26:26 -0800

    Updates to imports

--- lib/__init__.py
+++ lib/__init__.py
@@ -8,28 +8,14 @@ as well as utilities for reading and processing the data.
 # Import submodules
 from . import player, team, game
 
-# Re-export new names from submodules
-from .player import all as player_all, games_by_season, career_stats
-from .team import all as team_all, games as team_games
-from .game import boxscore, get_player_boxscores
 
 from .read_stats import read_stats
 
 __all__ = [
     # Submodules
-    'player',
-    'team',
-    'game',
-    # Player functions
-    'player_all',
-    'games_by_season',
-    'career_stats',
-    # Team functions
-    'team_all',
-    'team_games',
-    # Game functions
-    'boxscore',
-    'get_player_boxscores',
+    "player",
+    "team",
+    "game",
     # Utilities
-    'read_stats',
+    "read_stats",
 ]
